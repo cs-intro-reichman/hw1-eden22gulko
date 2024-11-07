0a2accf69ef299635e63b1662d4e6040870a37c3
Eden Sokolov Gulko <edensokolovgulko@Edens-MacBook-Air.local>
2024-11-06 19:39:01 -0800

    added 4 exercises in HW

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,13 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int money = Integer.parseInt(args[0]); 
+        double rate = Double.parseDouble(args[1]);
+        int years = Integer.parseInt(args[2]);
+        double newRate = rate / 100.0 ; 
+        double parentheses = ((newRate + 1));
+        double result =  money * (Math.pow(parentheses, years)); 
+        
+        System.out.println("After " + years + " years, " + "$" + money + " saved at " + rate + "%" + " will yield $" + (int)result);
 	}
 }
